bd1aa73fb6130348c5e6a7a23e62a47e1317ad3c
rhazama <rhazama01@gmail.com>
2022-10-21 09:55:56 -0700

    adding about

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import './App.css';
+import About from '/components/About';
 
 function App() {
   return (
     <div>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
