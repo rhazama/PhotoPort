cdfc0a067133e17f2338f03f69f3dfb73851e2d8
rhazama <rhazama01@gmail.com>
2022-10-21 11:30:28 -0700

    Adding Nav

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import About from './/components/About';
-import coverImage from "../../assets/cover/cover-image.jpg";
+import Nav from './components/Nav';
+import About from './components/About';
 
 function App() {
+  
   return (
     <div>
+      <Nav></Nav>
       <main>
-        <About>
-        <section className="my-5">
-          <h1 id="about">Who am I?</h1>
-          <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
-        </section>
-        </About>
+        <About></About>
       </main>
     </div>
   );
