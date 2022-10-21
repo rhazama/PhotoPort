26d701949187408d9e2bbe82ccd515360aeb447e
rhazama <rhazama01@gmail.com>
2022-10-21 11:22:20 -0700

    adding section to app function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import About from '/components/About';
+import About from './/components/About';
+import coverImage from "../../assets/cover/cover-image.jpg";
 
 function App() {
   return (
     <div>
       <main>
-        <About></About>
+        <About>
+        <section className="my-5">
+          <h1 id="about">Who am I?</h1>
+          <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+        </section>
+        </About>
       </main>
     </div>
   );
