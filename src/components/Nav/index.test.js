8e4736eabdc7c29f3434e014a66966d7306dfbe5
rhazama <rhazama01@gmail.com>
2022-10-21 18:38:43 -0700

    Adding emoji test to nav

--- src/components/Nav/index.test.js
+++ src/components/Nav/index.test.js
@@ -18,3 +18,12 @@ describe('Nav component', () => {
         expect(asFragment()).toMatchSnapshot();
       });
 })
+
+describe('emoji is visible', () => {
+    it('inserts emoji into the h2', () => {
+    const { getByLabelText } = render(<Nav />);
+  
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
+    });
+  })
+  
