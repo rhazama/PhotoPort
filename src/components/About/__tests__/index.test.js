7eb9a8daf25c75a30b2183e1ce04bef2a71cc3c1
rhazama <rhazama01@gmail.com>
2022-10-21 18:06:22 -0700

    Adding tomatchsnapshot

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-
 import About from '..';
 
 afterEach(cleanup);
@@ -11,7 +10,10 @@ describe('About component', () => {
     it('renders', () => {
       render(<About />);
     });
+    // Second Test
     it('matches snapshot DOM node structure', () => {
         // render About
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
       });
   })
