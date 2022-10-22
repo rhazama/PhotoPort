60f8f3c55f9efb519ed1571e7683f25f26996854
rhazama <rhazama01@gmail.com>
2022-10-21 18:48:35 -0700

    adjusting describe

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,3 +1,4 @@
+// __tests__/About.test.js
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
@@ -6,21 +7,13 @@ import About from '..';
 afterEach(cleanup);
 
 describe('About component', () => {
-    // First Test
-    it('renders', () => {
-      render(<About />);
-    });
-    // Second Test
-    it('matches snapshot DOM node structure', () => {
-        // render About
-        const { asFragment } = render(<About />);
-        expect(asFragment()).toMatchSnapshot();
-    });
-})
-
-describe('emoji is visible', () => {
-    it('inserts emoji into the h2', () => {
-    // Arrange
-    // Assert
-    })
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
