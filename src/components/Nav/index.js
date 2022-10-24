4bdb8d99a96cf93711b066d42717136c7cc7f673
rhazama <rhazama01@gmail.com>
2022-10-24 16:05:48 -0700

    Adding useState

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,45 +1,37 @@
 import React, { useState } from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
-function Nav() {
-const [categories] = useState([
-  {
-    name: 'commercial',
-    description: 'Photos of grocery stores, food trucks, and other commercial projects',
-  },
-  { name: 'portraits', description: 'Portraits of people in my life' },
-  { name: 'food', description: 'Delicious delicacies' },
-  { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
-]);
-const [currentCategory, setCurrentCategory] = useState(categories[0]);
+function Nav(props) {
+  const {
+    categories = [],
+    setCurrentCategory,
+    currentCategory,
+  } = props;
 
   return (
     <header className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
-          <span role="img" aria-label="camera">
-            {" "}
-            📸
-          </span>{" "}
-          Oh Snap!
+          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a
-              href="#about"
-            >
+            <a data-testid="about" href="#about">
               About me
             </a>
           </li>
-          <li>
+          <li className="mx-2">
             <span>Contact</span>
           </li>
           {categories.map((category) => (
-            <li className={`mx-1 ${
+            <li
+              className={`mx-1 ${
                 currentCategory.name === category.name && 'navActive'
-                }`} key={category.name}>
+                }`}
+              key={category.name}
+            >
               <span
                 onClick={() => {
                   setCurrentCategory(category)
@@ -54,5 +46,4 @@ const [currentCategory, setCurrentCategory] = useState(categories[0]);
     </header>
   );
 }
-
 export default Nav;
