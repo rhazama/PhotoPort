aa65ac4b8ef757f077fbd0aba95f5aaf151623c4
rhazama <rhazama01@gmail.com>
2022-10-24 15:48:05 -0700

    Adding description

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
-function Gallery() {
+function Gallery(props) {
+  const currentCategory = {
+    name: "commercial",
+    description:
+      "Photos of grocery stores, food trucks, and other commercial projects",
+  };
   return (
     <section>
-      <h1>NAME</h1>
-      <p>DESCRIPTION</p>
+      <h1>{currentCategory.name}</h1>
+      <p>{currentCategory.name}</p>
     </section>
   );
 }
