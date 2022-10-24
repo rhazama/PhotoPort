321a95fda5ed4bf52eac87b7e659380ce53918f7
rhazama <rhazama01@gmail.com>
2022-10-24 16:52:23 -0700

    fixing errors

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,15 +1,8 @@
-/* eslint-disable testing-library/prefer-screen-queries */
 // __tests__/Nav.test.js with hard coded categories
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
-
-const categories = [
-    { name: 'portraits', description: 'Portraits of people in my life' }
-  ]
-  const mockCurrentCategory = jest.fn();
-  const mockSetCurrentCategory = jest.fn();
 
 afterEach(cleanup);
 
@@ -20,18 +13,18 @@ describe('Nav component', () => {
 
   it('matches snapshot', () => {
     const { asFragment } = render(<Nav />);
-    
+
     expect(asFragment()).toMatchSnapshot();
   });
 })
 
 describe('emoji is visible', () => {
   it('inserts emoji into the h2', () => {
-  const { getByLabelText } = render(<Nav />);
+    const { getByLabelText } = render(<Nav />);
 
-  expect(getByLabelText('camera')).toHaveTextContent('📸');
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
   });
-})  
+})
 
 describe('links are visible', () => {
   it('inserts text into the links', () => {
