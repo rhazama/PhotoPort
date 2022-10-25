d4eef6ca99f156499bb52f98db6061eeaab71148
rhazama <rhazama01@gmail.com>
2022-10-24 17:26:26 -0700

    adding submit change

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,37 +3,37 @@ import React, { useState } from 'react';
 import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
-    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-  
-    const [errorMessage, setErrorMessage] = useState('');
-    const { name, email, message } = formState;
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      if (!errorMessage) {
-        setFormState({ [e.target.name]: e.target.value });
-        console.log('Form', formState);
-      }
-    };
-  
-    const handleChange = (e) => {
-      if (e.target.name === 'email') {
-        const isValid = validateEmail(e.target.value);
-        if (!isValid) {
-          setErrorMessage('Your email is invalid.');
-        } else {
-          setErrorMessage('');
-        }
-      } else {
-        if (!e.target.value.length) {
-          setErrorMessage(`${e.target.name} is required.`);
-        } else {
-          setErrorMessage('');
-        }
-      }
-    };
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
-return (
+  const [errorMessage, setErrorMessage] = useState('');
+  const { name, email, message } = formState;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      setFormState({ [e.target.name]: e.target.value });
+      console.log('Form', formState);
+    }
+  };
+
+  const handleChange = (e) => {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+  };
+
+  return (
     <section>
       <h1 data-testid="h1tag">Contact me</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
