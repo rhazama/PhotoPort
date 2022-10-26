24694db89d9d4919ce14636c30d37156081aee39
rhazama <rhazama01@gmail.com>
2022-10-25 21:44:44 -0700

    adding image and button

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -6,6 +6,13 @@ function Modal() {
       <div className="modalBackdrop">
         <div className="modalContainer">
         <h3 className="modalTitle">Photo Name</h3>
+        <img alt="current category" />
+        <p>
+          Photo Description
+        </p>
+        <button type="button">
+          Close this modal
+        </button>
         </div>
       </div>
 
