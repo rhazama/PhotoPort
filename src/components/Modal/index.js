17ee6e3f564dec8c43c7cd8c954195ddf80194ed
rhazama <rhazama01@gmail.com>
2022-10-28 11:25:54 -0700

    fixing return

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,21 +1,14 @@
 import React from 'react';
 
-function Modal() {
-
-  const [currentPhoto, setCurrentPhoto] = useState();
-  const toggleModal = (image, i) => {
-    setCurrentPhoto({...image, index: i});
-  }
-  <Modal currentPhoto={currentPhoto}></Modal>
+function Modal({currentPhoto}) {
+ const {name, category, description, index} = currentPhoto;
 
     return (
       <div className="modalBackdrop">
-        <div className="modalContainer">
-        <h3 className="modalTitle">Photo Name</h3>
-        <img alt="current category" />
-        <p>
-          Photo Description
-        </p>
+      <div className="modalContainer">
+        <h3 className="modalTitle">{name}</h3>
+        <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
+        <p>{description}</p>
         <button type="button">
           Close this modal
         </button>
