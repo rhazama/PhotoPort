8834b207b8b283f2b6deb87b22bc75f3fb9f55fb
rhazama <rhazama01@gmail.com>
2022-10-28 11:30:49 -0700

    adjusting img

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -7,7 +7,10 @@ function Modal({currentPhoto}) {
       <div className="modalBackdrop">
       <div className="modalContainer">
         <h3 className="modalTitle">{name}</h3>
-        <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
+        <img 
+          src={require(`../../assets/large/${category}/${index}.jpg`).default}
+          alt="current category"
+        />
         <p>{description}</p>
         <button type="button">
           Close this modal
